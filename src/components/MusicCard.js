ffbaabbf990ca9dfa2e1c8591ed01f35cf155e83
NiiVx <vinicios241@gmail.com>
2022-03-19 14:30:33 -0300

    Test

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -4,17 +4,31 @@ import './MusicCard.css';
 
 export default class MusicCard extends React.Component {
   render() {
-    const { trackName, previewUrl } = this.props;
+    const {
+      trackName, previewUrl, trackId, favoriteCheck, onFavoriteChange } = this.props;
     return (
       <div className="parent">
         <h3>{trackName}</h3>
         <audio data-testid="audio-component" src={ previewUrl } controls>
           <track kind="captions" />
         </audio>
+        <label htmlFor="Favorita">
+          Favorita
+          <input
+            data-testid={ `checkbox-music-${trackId}` }
+            type="checkbox"
+            name="Favorita"
+            checked={ favoriteCheck }
+            onChange={ onFavoriteChange }
+          />
+        </label>
       </div>);
   }
 }
 MusicCard.propTypes = {
   trackName: PropTypes.string.isRequired,
   previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.string.isRequired,
+  onFavoriteChange: PropTypes.func.isRequired,
+  favoriteCheck: PropTypes.bool.isRequired,
 };
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
+import { addSong } from '../services/favoriteSongsAPI';
 import MusicCard from '../components/MusicCard';
 
 export default class Album extends React.Component {
@@ -12,6 +13,7 @@ export default class Album extends React.Component {
       musics: [],
       nameArtist: '',
       nameAlbum: '',
+      favoriteCheck: false,
     };
   }
 
@@ -23,10 +25,20 @@ export default class Album extends React.Component {
       nameArtist: resultApi.at(0).artistName,
       nameAlbum: resultApi.at(0).collectionName,
     });
+    await addSong({ song: `${resultApi.at(0).trackId}` });
+  }
+
+  onFavoriteChange = ({ target }) => {
+    const { name } = target;
+    console.log(name);
+    this.setState({ favoriteCheck: true });
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value });
   }
 
   render() {
-    const { musics, nameAlbum, nameArtist } = this.state;
+    const { musics, nameAlbum, nameArtist, favoriteCheck } = this.state;
     return (
       <div>
         <Header />
@@ -36,11 +48,14 @@ export default class Album extends React.Component {
           {<p data-testid="artist-name">{nameArtist}</p>}
           {<p data-testid="album-name">{nameAlbum}</p>}
           <section>
-            {musics.slice(1).map((music) => (
+            {musics.slice(1).map((music, index) => (
               <MusicCard
-                key={ music.artistId }
+                key={ index }
                 trackName={ music.trackName }
                 previewUrl={ music.previewUrl }
+                trackId={ music.trackId }
+                checked={ favoriteCheck }
+                onclick={ this.onFavoriteChange }
               />
             ))}
           </section>
